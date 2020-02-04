8e3fb32a231cbc034dc0def97c03df24ffd8d4ff
skyrunner360 <mathurrishi31@gmail.com>
2020-02-04 19:41:50 +0530

    comments

--- secrets.py
+++ secrets.py
@@ -1,7 +1,7 @@
-link = 'https://docs.google.com/forms/d/e/1FAIpQLSe49i8NIJRYzuDeZbc6bDMC9XL_oV-dx2UlxhydnbrfkVFUpA/viewform'
-email = 'rishi.mathur@adypu.edu.in'
-passwd = 'mrZ93TEqNEHn'
-name='Rishi Mathur'
-urn='2018-B-13091999A'
+link = 'https://docs.google.com/forms/d/e/1FAIpQLSe49i8NIJRYzuDeZbc6bDMC9XL_oV-dx2UlxhydnbrfkVFUpA/viewform' #Enter Form Link Here
+email = '' #Enter Your ADYPU email id Here
+passwd = '' #Enter Your ADYPU email id Password Here
+name='' #Enter Your Full Name Here
+urn='' #Enter Your URN Here
 cmnt='' #Enter comment to be given on every page
         #NOTE - You cannot change the comment individually
